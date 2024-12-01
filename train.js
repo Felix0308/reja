2c9463076cc78668d85115edb441ebe24b3aa01e
felix0308 <utkirbekm.0327@gmail.com>
2024-12-01 17:43:08 +0900

    feat: D-TASK

--- train.js
+++ train.js
@@ -215,10 +215,70 @@
 // Shunday function tuzing, u 2ta string parametr ega bolsin, hamda agar har ikkala string bir hil harflardan iborat bolsa true aks holda false qaytarsin
 // MASALAN checkContent("mitgroup", "gmtiprou") return qiladi true;
 
-function checkContent(str1, str2) {
-  return str1.split("").sort().join("") === str2.split("").sort().join("");
+// function checkContent(str1, str2) {
+//   return str1.split("").sort().join("") === str2.split("").sort().join("");
+// }
+
+// // call
+// console.log(checkContent("mitgroup", "gmtiprou"));  // true
+// console.log(checkContent("hello", "world"));        // false
+
+// ***********************************************************************************************************
+// D-TASK: 
+
+// Shunday class tuzing tuzing nomi Shop, va uni constructoriga 3 hil mahsulot pass bolsin, hamda classning 3ta methodi bolsin, 
+// biri qoldiq, biri sotish va biri qabul. Har bir method ishga tushgan vaqt ham log qilinsin.
+// MASALAN: const shop = new Shop(4, 5, 2); shop.qoldiq() return hozir 20:40da 4ta non, 5ta lagmon va 2ta cola mavjud! 
+// shop.sotish('non', 3) & shop.qabul('cola', 4) & shop.qoldiq() return hozir 20:50da 1ta non, 5ta lagmon va 6ta cola mavjud!
+
+class Shop {
+  constructor(non, lagmon, cola) {
+    this.products = { non, lagmon, cola };
+  }
+
+  logTime() {
+    const now = new Date();
+    return `hozir ${now.getHours()}:${now.getMinutes() < 10 ? '0' : ''}${now.getMinutes()}da`;
+  }
+
+  qoldiq() {
+    const { non, lagmon, cola } = this.products;
+    console.log(
+      `${this.logTime()} ${non}ta non, ${lagmon}ta lagmon va ${cola}ta cola mavjud!`
+    );
+    return { ...this.products };
+  }
+
+  sotish(tur, miqdor) {
+    if (!this.products[tur]) {
+      console.log(`${this.logTime()} ${tur} mahsuloti mavjud emas!`);
+      return;
+    }
+
+    if (this.products[tur] < miqdor) {
+      console.log(`${this.logTime()} yetarli ${tur} mahsuloti yo'q!`);
+      return;
+    }
+
+    this.products[tur] -= miqdor;
+    console.log(`${this.logTime()} ${miqdor}ta ${tur} sotildi!`);
+  }
+
+  qabul(tur, miqdor) {
+    if (!this.products[tur]) {
+      console.log(`${this.logTime()} ${tur} mahsuloti do'konda yo'q!`);
+      return;
+    }
+
+    this.products[tur] += miqdor;
+    console.log(`${this.logTime()} ${miqdor}ta ${tur} qabul qilindi!`);
+  }
 }
 
-// call
-console.log(checkContent("mitgroup", "gmtiprou"));  // true
-console.log(checkContent("hello", "world"));        // false
+// Foydalanish
+const shop = new Shop(4, 5, 2);
+
+shop.qoldiq();           // Masalan: hozir 20:40da 4ta non, 5ta lagmon va 2ta cola mavjud!
+shop.sotish("non", 3);   // Masalan: hozir 20:42da 3ta non sotildi!
+shop.qabul("cola", 4);   // Masalan: hozir 20:45da 4ta cola qabul qilindi!
+shop.qoldiq();           // Masalan: hozir 20:50da 1ta non, 5ta lagmon va 6ta cola mavjud!
