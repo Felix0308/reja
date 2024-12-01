e98226b89b26f7692bb73fad0cb44335c24bad36
felix0308 <utkirbekm.0327@gmail.com>
2024-12-01 17:53:49 +0900

    feat: D-TASK.

--- train.js
+++ train.js
@@ -231,54 +231,93 @@
 // MASALAN: const shop = new Shop(4, 5, 2); shop.qoldiq() return hozir 20:40da 4ta non, 5ta lagmon va 2ta cola mavjud! 
 // shop.sotish('non', 3) & shop.qabul('cola', 4) & shop.qoldiq() return hozir 20:50da 1ta non, 5ta lagmon va 6ta cola mavjud!
 
+// class Shop {
+//   constructor(non, lagmon, cola) {
+//     this.products = { non, lagmon, cola };
+//   }
+
+//   logTime() {
+//     const now = new Date();
+//     return `hozir ${now.getHours()}:${now.getMinutes() < 10 ? '0' : ''}${now.getMinutes()}da`;
+//   }
+
+//   qoldiq() {
+//     const { non, lagmon, cola } = this.products;
+//     console.log(
+//       `${this.logTime()} ${non}ta non, ${lagmon}ta lagmon va ${cola}ta cola mavjud!`
+//     );
+//     return { ...this.products };
+//   }
+
+//   sotish(tur, miqdor) {
+//     if (!this.products[tur]) {
+//       console.log(`${this.logTime()} ${tur} mahsuloti mavjud emas!`);
+//       return;
+//     }
+
+//     if (this.products[tur] < miqdor) {
+//       console.log(`${this.logTime()} yetarli ${tur} mahsuloti yo'q!`);
+//       return;
+//     }
+
+//     this.products[tur] -= miqdor;
+//     console.log(`${this.logTime()} ${miqdor}ta ${tur} sotildi!`);
+//   }
+
+//   qabul(tur, miqdor) {
+//     if (!this.products[tur]) {
+//       console.log(`${this.logTime()} ${tur} mahsuloti do'konda yo'q!`);
+//       return;
+//     }
+
+//     this.products[tur] += miqdor;
+//     console.log(`${this.logTime()} ${miqdor}ta ${tur} qabul qilindi!`);
+//   }
+// }
+
+// // Foydalanish
+// const shop = new Shop(4, 5, 2);
+
+// shop.qoldiq();           // Masalan: hozir 20:40da 4ta non, 5ta lagmon va 2ta cola mavjud!
+// shop.sotish("non", 3);   // Masalan: hozir 20:42da 3ta non sotildi!
+// shop.qabul("cola", 4);   // Masalan: hozir 20:45da 4ta cola qabul qilindi!
+// shop.qoldiq();           // Masalan: hozir 20:50da 1ta non, 5ta lagmon va 6ta cola mavjud!
+
+// 2 -usul
 class Shop {
   constructor(non, lagmon, cola) {
     this.products = { non, lagmon, cola };
   }
 
-  logTime() {
+  log(message) {
     const now = new Date();
-    return `hozir ${now.getHours()}:${now.getMinutes() < 10 ? '0' : ''}${now.getMinutes()}da`;
+    console.log(`[${now.getHours()}:${now.getMinutes()}] ${message}`);
   }
 
   qoldiq() {
     const { non, lagmon, cola } = this.products;
-    console.log(
-      `${this.logTime()} ${non}ta non, ${lagmon}ta lagmon va ${cola}ta cola mavjud!`
-    );
-    return { ...this.products };
+    this.log(`${non}ta non, ${lagmon}ta lagmon, ${cola}ta cola mavjud!`);
   }
 
   sotish(tur, miqdor) {
-    if (!this.products[tur]) {
-      console.log(`${this.logTime()} ${tur} mahsuloti mavjud emas!`);
-      return;
+    if (this.products[tur] >= miqdor) {
+      this.products[tur] -= miqdor;
+      this.log(`${miqdor}ta ${tur} sotildi!`);
+    } else {
+      this.log(`Yetarli ${tur} yo'q!`);
     }
-
-    if (this.products[tur] < miqdor) {
-      console.log(`${this.logTime()} yetarli ${tur} mahsuloti yo'q!`);
-      return;
-    }
-
-    this.products[tur] -= miqdor;
-    console.log(`${this.logTime()} ${miqdor}ta ${tur} sotildi!`);
   }
 
   qabul(tur, miqdor) {
-    if (!this.products[tur]) {
-      console.log(`${this.logTime()} ${tur} mahsuloti do'konda yo'q!`);
-      return;
-    }
-
     this.products[tur] += miqdor;
-    console.log(`${this.logTime()} ${miqdor}ta ${tur} qabul qilindi!`);
+    this.log(`${miqdor}ta ${tur} qabul qilindi!`);
   }
 }
 
 // Foydalanish
 const shop = new Shop(4, 5, 2);
 
-shop.qoldiq();           // Masalan: hozir 20:40da 4ta non, 5ta lagmon va 2ta cola mavjud!
-shop.sotish("non", 3);   // Masalan: hozir 20:42da 3ta non sotildi!
-shop.qabul("cola", 4);   // Masalan: hozir 20:45da 4ta cola qabul qilindi!
-shop.qoldiq();           // Masalan: hozir 20:50da 1ta non, 5ta lagmon va 6ta cola mavjud!
+shop.qoldiq();         // Masalan: [20:40] 4ta non, 5ta lagmon, 2ta cola mavjud!
+shop.sotish('non', 3); // Masalan: [20:41] 3ta non sotildi!
+shop.qabul('cola', 4); // Masalan: [20:42] 4ta cola qabul qilindi!
+shop.qoldiq();         // Masalan: [20:43] 1ta non, 5ta lagmon, 6ta cola mavjud!
